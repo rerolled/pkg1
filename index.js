bc028e0c6f13fdfd865578c5d44c4fc2250f7f50
mike sizz <mikesizz@mikes-MacBook-Pro.local>
2017-05-11 15:53:37 -0400

    altering dummy methods

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'spendMoney': function(){},
-	'getSavings': function(){}
+	'spendMoney': function(){ console.log('pkg1 spending money'); },
+	'getSavings': function(){ console.log('pkg1 getting savings'); }
 };
